d8308c2bcd2fd1692b4dcadef90ec4adb99a219d
Jin <fjkl.446@gmail.com>
2022-04-12 18:54:29 +0900

    Add files via upload

---
+++ pycker/run.py
@@ -0,0 +1,6 @@
+import time
+
+if __name__ == '__main__':
+    print('Running python docker')
+    while True:
+        time.sleep(10**5)
---
+++ pycker/src/app.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+print(np.array([0, 0]))
+print('hello ')
